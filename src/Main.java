ab47d2e5ad95638df3a979925520bfd9088ee25d
Salvatore Catania <salvatore.catania@live.it>
2019-10-28 13:11:33 +0100

    Initial commit..

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,58 @@ import java.util.Arrays;
 
 public class Main {
 	
+	//ar[] sotto-vettore da ordinare
+	//n lunghezza sottovettore
+	//i Indice root
+	public void heapify(int ar[], int n, int i) {
+		//1° step identificare attori processo algoritmo
+		int largest = i;
+		int left = i*2 + 1;
+		int right = i*2 + 2;
+		
+		//2° step verificare che left e right siano effettivamente minori di root (minori perchè stiamo realizzando un Max-Heap)
+		//Verifichiamo prima che il figlio sinistro sia effettivamente più piccolo del suo parent
+		//altrimenti modifichiamo l'indice
+		if(left < n && ar[left] > ar[largest])
+			largest = left;
+		//Effettuiamo la stessa operazione sul figlio destro
+		if(right < n && ar[right] > ar[largest])
+			largest = right;
+		
+		//Se alla fine dell'algoritmo non l'indice del valore maggiore non corrisponde con l'indice della root
+		//Si aggiorna l'heap e ricorsivamente lo si rimanda al processo di heapify
+		if(largest != i) {
+			int app = ar[i];
+			ar[i] = ar[largest];
+			ar[largest] = app;
+			
+			heapify(ar, n, largest);
+		}
+	}
+	
 	public void sort(int ar[]) {
 		int n = ar.length;
 		
-        // Build heap (rearrange array) 
-		for(int i = n/2 - 1; i >= 0; i--)
+		//Costruisco l'albero o heap
+		for(int i = n /2 - 1; i >= 0; i--)
 			heapify(ar, n, i);
-
-		// One by one extract an element from heap 
+		
+		//Uno a uno esco gli elementi dall'heap e li ordino partendo dall radice
 		for(int i = n - 1; i >= 0; i--) {
-            // Move current root to end 
-			int temp = ar[0];
-			ar[0] = ar[i];
-			ar[i] = temp;
+			int app = ar[0];
+			ar[i] = ar[0];
+			ar[i] = app;
 			
-			// call max heapify on the reduced heap
 			heapify(ar, i, 0);
 		}
-	}
-	
-	// To heapify a subtree rooted with node i which is 
-    // an index in arr[]. n is size of heap 
-	public void heapify(int ar[], int n, int i) {
-		int largest = i; 		//Initialize largest as root
-		int left = 2*i + 1;		//left = 2*i + 1
-		int rigth = 2*i + 2;	//rigth = 2*i + 1
 		
-		//If left child is greater then largest swap
-		if(left < n && ar[left] > ar[largest])
-			largest = left;
-		
-		//If rigth child is greater then largest swap
-		if(rigth < n && ar[rigth] > ar[largest])
-			largest = rigth;
-		
-		//If largest is not root
-		if(largest != i) {
-			int swap = ar[i];
-			ar[i] = ar[largest];
-			ar[largest] = swap;
-			
-			// Recursively heapify the affected sub-tree 
-            heapify(ar, n, largest); 
-		}
 	}
 	
 	public static void main(String[] args) {
-		Main main = new Main();
-		int ar[] = {3,6,19,4,5,0,8,2};
-		main.sort(ar);
-		System.out.print(Arrays.toString(ar));
+//		Main main = new Main();
+//		int ar[] = {3,6,19,4,5,0,8,2};
+//		main.sort(ar);
+//		System.out.print(Arrays.toString(ar));
 	}
 
 }
